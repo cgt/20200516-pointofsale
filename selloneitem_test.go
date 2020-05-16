3ca818686186e38fa29ca3c3d6faeee9e4fa4f51
Christoffer G. Thomsen <chris@cgt.name>
2020-05-16 12:47:21 +0200

    Remove redundant, non-table-driven TestSellOneItem suite

--- selloneitem_test.go
+++ selloneitem_test.go
@@ -13,58 +13,6 @@ func (d *spyDisplay) Display(text string) {
 	d.currentText = text
 }
 
-func TestSellOneItem(t *testing.T) {
-	catalog := map[string]string{
-		"12345": "$6.78",
-		"11223": "$5.00",
-	}
-
-	t.Run("product found", func(t *testing.T) {
-		display := &spyDisplay{}
-
-		sale := &Sale{display, catalog}
-		sale.OnBarcode("12345\n")
-
-		assert.Equal(t, "$6.78", display.currentText)
-	})
-
-	t.Run("another product found", func(t *testing.T) {
-		display := &spyDisplay{}
-
-		sale := &Sale{display, catalog}
-		sale.OnBarcode("11223\n")
-
-		assert.Equal(t, "$5.00", display.currentText)
-	})
-
-	t.Run("product not found", func(t *testing.T) {
-		display := &spyDisplay{}
-
-		sale := &Sale{display, catalog}
-		sale.OnBarcode("::no such product::\n")
-
-		assert.Equal(t, "product not found", display.currentText)
-	})
-
-	t.Run("empty barcode", func(t *testing.T) {
-		display := &spyDisplay{}
-
-		sale := &Sale{display, catalog}
-		sale.OnBarcode("")
-
-		assert.Equal(t, "error: invalid barcode", display.currentText)
-	})
-
-	t.Run("blank barcode", func(t *testing.T) {
-		display := &spyDisplay{}
-
-		sale := &Sale{display, catalog}
-		sale.OnBarcode("\n")
-
-		assert.Equal(t, "error: invalid barcode", display.currentText)
-	})
-}
-
 func TestSellOneItemTable(t *testing.T) {
 	testCases := []struct {
 		description         string
