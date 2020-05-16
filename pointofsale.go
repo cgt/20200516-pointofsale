0a52640bd77a9818af493eb9ed0339f2f1c76585
Christoffer G. Thomsen <chris@cgt.name>
2020-05-16 12:40:04 +0200

    Remove duplication

--- pointofsale.go
+++ pointofsale.go
@@ -12,11 +12,12 @@ type Sale struct {
 }
 
 func (s *Sale) OnBarcode(barcode string) {
-	if strings.TrimSpace(barcode) == "" {
+	barcode = strings.TrimSpace(barcode)
+	if barcode == "" {
 		s.display.Display("error: invalid barcode")
 		return
 	}
-	if price, ok := s.pricesByBarcode[strings.TrimSpace(barcode)]; ok {
+	if price, ok := s.pricesByBarcode[barcode]; ok {
 		s.display.Display(price)
 	} else {
 		s.display.Display("product not found")
