f4c59843c1ed60ce1359d3534ec6493e2ac03b31
Christoffer G. Thomsen <chris@cgt.name>
2020-05-16 12:46:25 +0200

    Rewrite TestSellOneItem as table-driven tests

--- selloneitem_test.go
+++ selloneitem_test.go
@@ -64,3 +64,47 @@ func TestSellOneItem(t *testing.T) {
 		assert.Equal(t, "error: invalid barcode", display.currentText)
 	})
 }
+
+func TestSellOneItemTable(t *testing.T) {
+	testCases := []struct {
+		description         string
+		barcode             string
+		expectedDisplayText string
+	}{
+		{
+			description:         "product found",
+			barcode:             "12345\n",
+			expectedDisplayText: "$6.78",
+		},
+		{
+			description:         "another product found",
+			barcode:             "11223\n",
+			expectedDisplayText: "$5.00",
+		},
+		{
+			description:         "empty barcode",
+			barcode:             "",
+			expectedDisplayText: "error: invalid barcode",
+		},
+		{
+			description:         "blank barcode",
+			barcode:             "\n",
+			expectedDisplayText: "error: invalid barcode",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			display := &spyDisplay{}
+			catalog := map[string]string{
+				"12345": "$6.78",
+				"11223": "$5.00",
+			}
+
+			sale := &Sale{display, catalog}
+			sale.OnBarcode(tc.barcode)
+
+			assert.Equal(t, tc.expectedDisplayText, display.currentText)
+		})
+	}
+}
