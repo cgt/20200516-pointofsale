6b99a15741e6a4f810bf6d465bf84304ea99855a
Christoffer G. Thomsen <chris@cgt.name>
2020-05-16 11:40:48 +0200

    Sale displays price $6.78 when product 12345 scanned

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module pointofsale
 
 go 1.14
 
-require github.com/stretchr/testify v1.5.1 // indirect
+require github.com/stretchr/testify v1.5.1
--- selloneitem_test.go
+++ selloneitem_test.go
@@ -1,6 +1,37 @@
 package pointofsale
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type Display interface {
+	Display(text string)
+}
+
+type spyDisplay struct {
+	currentText string
+}
+
+func (d *spyDisplay) Display(text string) {
+	d.currentText = text
+}
+
+type Sale struct {
+	display Display
+}
+
+func (s *Sale) OnBarcode(barcode string) {
+	s.display.Display("$6.78")
+}
 
 func TestSellOneItem(t *testing.T) {
+	t.Run("product found", func(t *testing.T) {
+		display := &spyDisplay{}
+
+		sale := &Sale{display}
+		sale.OnBarcode("12345")
+
+		assert.Equal(t, "$6.78", display.currentText)
+	})
 }
