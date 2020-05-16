a28e3279e83279820079deef21dcfeb0db678b4a
Christoffer G. Thomsen <chris@cgt.name>
2020-05-16 16:18:32 +0200

    InMemoryCatalog formats cent prices

--- pointofsale.go
+++ pointofsale.go
@@ -1,6 +1,9 @@
 package pointofsale
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 type Catalog interface {
 	FormattedPrice(barcode string) (price string, ok bool)
@@ -40,6 +43,7 @@ func (s InMemoryCatalog) FormattedPrice(barcode string) (string, bool) {
 		priceInCents, ok = s.pricesInCentsByBarcode[barcode]
 		_ = priceInCents
 		price = "$5.21"
+		price = fmt.Sprintf("$%v", float64(priceInCents)/100.0)
 	}
 	return price, ok
 }
