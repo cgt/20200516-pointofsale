0f374ffac0c1f9e86fa748bdfaffbac3df2db6cd
Christoffer G. Thomsen <chris@cgt.name>
2020-05-16 12:25:33 +0200

    Inject copy of hardcoded prices map in the tests
    Duplicates the hardcoded map in OnBarcode.

--- selloneitem_test.go
+++ selloneitem_test.go
@@ -17,7 +17,10 @@ func TestSellOneItem(t *testing.T) {
 	t.Run("product found", func(t *testing.T) {
 		display := &spyDisplay{}
 
-		sale := &Sale{display, nil}
+		sale := &Sale{display, map[string]string{
+			"12345": "$6.78",
+			"11223": "$5.00",
+		}}
 		sale.OnBarcode("12345\n")
 
 		assert.Equal(t, "$6.78", display.currentText)
@@ -26,7 +29,10 @@ func TestSellOneItem(t *testing.T) {
 	t.Run("another product found", func(t *testing.T) {
 		display := &spyDisplay{}
 
-		sale := &Sale{display, nil}
+		sale := &Sale{display, map[string]string{
+			"12345": "$6.78",
+			"11223": "$5.00",
+		}}
 		sale.OnBarcode("11223\n")
 
 		assert.Equal(t, "$5.00", display.currentText)
