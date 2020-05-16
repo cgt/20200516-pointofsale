95be6cb68e5ada9637a3bc1fd2e2078e8e853ec2
Christoffer G. Thomsen <chris@cgt.name>
2020-05-16 12:22:53 +0200

    Move Sale and Display out of test file

---
+++ pointofsale.go
@@ -0,0 +1,28 @@
+package pointofsale
+
+import "strings"
+
+type Display interface {
+	Display(text string)
+}
+
+type Sale struct {
+	display         Display
+	pricesByBarcode map[string]string
+}
+
+func (s *Sale) OnBarcode(barcode string) {
+	pricesByBarcode := map[string]string{
+		"12345": "$6.78",
+		"11223": "$5.00",
+	}
+	if price, ok := s.pricesByBarcode[strings.TrimSpace(barcode)]; ok {
+		s.display.Display(price)
+	} else {
+		if price, ok := pricesByBarcode[strings.TrimSpace(barcode)]; ok {
+			s.display.Display(price)
+		} else {
+			s.display.Display("product not found")
+		}
+	}
+}
--- selloneitem_test.go
+++ selloneitem_test.go
@@ -2,13 +2,8 @@ package pointofsale
 
 import (
 	"github.com/stretchr/testify/assert"
-	"strings"
 	"testing"
 )
-
-type Display interface {
-	Display(text string)
-}
 
 type spyDisplay struct {
 	currentText string
@@ -16,27 +11,6 @@ type spyDisplay struct {
 
 func (d *spyDisplay) Display(text string) {
 	d.currentText = text
-}
-
-type Sale struct {
-	display         Display
-	pricesByBarcode map[string]string
-}
-
-func (s *Sale) OnBarcode(barcode string) {
-	pricesByBarcode := map[string]string{
-		"12345": "$6.78",
-		"11223": "$5.00",
-	}
-	if price, ok := s.pricesByBarcode[strings.TrimSpace(barcode)]; ok {
-		s.display.Display(price)
-	} else {
-		if price, ok := pricesByBarcode[strings.TrimSpace(barcode)]; ok {
-			s.display.Display(price)
-		} else {
-			s.display.Display("product not found")
-		}
-	}
 }
 
 func TestSellOneItem(t *testing.T) {
